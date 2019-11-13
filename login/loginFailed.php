deda43a78cf744759adf1abbb6dcfea6a1496dde
brianndjojo <38829497+brianndjojo@users.noreply.github.com>
2019-11-14 04:42:37 +0800

    Basic login done. [14/11/19]

---
+++ login/loginFailed.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="reset.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
+    <script src="process.php"></script>
+</head>
+<body>
+    <div id="login">
+        <form action = "process.php" id = "loginForm" method = "POST">
+            <p>
+                <label>Username:</label>
+                <input type = "text" id = "userText" name = "username" />
+            </p>
+
+            <p>
+                <label>Password:</label>
+                <input type = "password" id = "passwordText" name = "password" />
+            </p>
+
+            <input type = "submit" id = "submitButton" value = "Login" />
+            
+            <input type = "submit" id = "registerButton" value = "Register" />
+        </form>
+        
+    </div> 
+    <div id = "failedMessage">
+        <p>Invalid username or password.</p>
+    </div>  
+     
+</body>
+</html>
