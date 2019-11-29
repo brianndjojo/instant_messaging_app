460bd3f17f134c3b93e2550f20923e90e3a424b4
brianndjojo <38829497+brianndjojo@users.noreply.github.com>
2019-11-30 03:30:07 +0800

    [30/11/19] Fixed Login Error & Used Bootstrap.
    - Changed design & used Bootstrap for responsiveness.
    - Fixed a Login Error.
    - Fixed Registration button.

--- login/login.php
+++ login/login.php
@@ -3,30 +3,39 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
+    <title>User Logins</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="reset.css" />
-    <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="css/main/reset.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="css/main/style.css" />
+    
     <script src="process.php"></script>
 </head>
 <body>
     <div id="login">
         <form action = "process.php" id = "loginForm" method = "POST">
-            <p>
-                <label>Username:</label>
-                <input type = "text" id = "userText" name = "username" />
-            </p>
+           
+            <div class = "container">
+                <div class = "row">
+                    <div class = "col-xs-3">
+                        <h1>User Login</h1>
 
-            <p>
-                <label>Password:</label>
-                <input type = "password" id = "passwordText" name = "password" />
-            </p>
+                        <hr class = "mb-3">
 
-            <input type = "submit" id = "submitButton" value = "Login" />
+                        <label><b>Username</b></label>
+                        <input class = "form-control" id = "userText" type = "text" name = "username" required>
+
+                        <label><b>Password</b></label>
+                        <input class = "form-control" id = "passwordText" type = "password" name = "password" required>
+                        
+                        <hr class = "mb-3">
+
+                        <input type = "submit" class="btn btn-outline-dark" id = "submitButton" value = "Login" />
             
-            <input type = "submit" id = "registerButton" value = "Register" />
-            
-
+                        <a href = "http://localhost/register/registration.php"><input type = "submit" class="btn btn-outline-dark" id = "registerButton" value = "Register"  /></a>
+                    </div>
+                </div>
+            </div>
         </form>
     </div>    
 </body>
--- login/process.php
+++ login/process.php
@@ -24,19 +24,64 @@
     $info_result = mysql_query(" SELECT * FROM `users` WHERE username = '$userName' " )
                         or die("Failed to query the database. ".mysql_error());
     $row = mysql_fetch_array($info_result);
-
-    $fetchedKey = Key::loadFromAsciiSafeString($row['userKey']);
-    $storedPass = Crypto::decrypt($row['password'], $fetchedKey, $raw_binary = false);
-    
-    if($row['username'] == $userName && $passWord == $storedPass && $row['username'] != "" && $row['password'] != "")
+    if($row['username'] == $userName)
     {
-        echo "Welcome ".$row['username'];
-    }
-    else 
-    {
-        header("Location:loginFailed.php");
-        exit();
+        $fetchedKey = Key::loadFromAsciiSafeString($row['userKey']);
+        $storedPass = Crypto::decrypt($row['password'], $fetchedKey, $raw_binary = false);
         
+        if($passWord == $storedPass && $row['username'] != "" && $row['password'] != "")
+        {
+        
+        echo "Login Success!";
+        
+        }
     }
+    
+    //Generates Error msg by default as login was not successful.
+    echo '<div class="errorMsg"> Invalid Username or Password! </div>';
+        
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>User Logins</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/main/reset.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="css/main/style.css" />
+    
+    <script src="process.php"></script>
+</head>
+<body>
+    <div id="login">
+        <form action = "process.php" id = "loginForm" method = "POST">
+           
+            <div class = "container">
+                <div class = "row">
+                    <div class = "col-xs-3">
+                        <h1>User Login</h1>
+
+                        <hr class = "mb-3">
+
+                        <label><b>Username</b></label>
+                        <input class = "form-control" id = "userText" type = "text" name = "username" required>
+
+                        <label><b>Password</b></label>
+                        <input class = "form-control" id = "passwordText" type = "password" name = "password" required>
+                        
+                        <hr class = "mb-3">
+
+                        <input type = "submit" class="btn btn-outline-dark" id = "submitButton" value = "Login" />
+            
+                        <a href = "http://localhost/register/registration.php"><input type = "submit" class="btn btn-outline-dark" id = "registerButton" value = "Register"  /></a>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>    
+</body>
+</html>
 
